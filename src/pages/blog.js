215c2bbc08b833ff8e31dbb28581fbd35c4f971b
Leithon English <leithonenglish@gmail.com>
2022-06-27 11:25:11 -0500

    changed over to static queries

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { graphql, StaticQuery } from 'gatsby'
 import get from 'lodash/get'
 
 import Seo from '../components/seo'
@@ -9,13 +9,42 @@ import ArticlePreview from '../components/article-preview'
 
 class BlogIndex extends React.Component {
   render() {
-    const posts = get(this, 'props.data.allContentfulBlogPost.nodes')
+    // const posts = get(this, 'props.data.allContentfulBlogPost.nodes')
 
     return (
       <Layout location={this.props.location}>
         <Seo title="Blog" />
         <Hero title="Blog" />
-        <ArticlePreview posts={posts} />
+        <StaticQuery
+          query={graphql`
+            query BlogIndexQuery {
+              allContentfulBlogPost(
+                sort: { fields: [publishDate], order: DESC }
+              ) {
+                nodes {
+                  title
+                  slug
+                  publishDate(formatString: "MMMM Do, YYYY")
+                  tags
+                  heroImage {
+                    gatsbyImageData(
+                      layout: FULL_WIDTH
+                      placeholder: BLURRED
+                      width: 424
+                      height: 212
+                    )
+                  }
+                  description {
+                    raw
+                  }
+                }
+              }
+            }
+          `}
+          render={(data) => (
+            <ArticlePreview posts={data.allContentfulBlogPost.nodes} />
+          )}
+        />
       </Layout>
     )
   }
@@ -23,26 +52,26 @@ class BlogIndex extends React.Component {
 
 export default BlogIndex
 
-export const pageQuery = graphql`
-  query BlogIndexQuery {
-    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
-      nodes {
-        title
-        slug
-        publishDate(formatString: "MMMM Do, YYYY")
-        tags
-        heroImage {
-          gatsbyImageData(
-            layout: FULL_WIDTH
-            placeholder: BLURRED
-            width: 424
-            height: 212
-          )
-        }
-        description {
-          raw
-        }
-      }
-    }
-  }
-`
+// export const pageQuery = graphql`
+//   query BlogIndexQuery {
+//     allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
+//       nodes {
+//         title
+//         slug
+//         publishDate(formatString: "MMMM Do, YYYY")
+//         tags
+//         heroImage {
+//           gatsbyImageData(
+//             layout: FULL_WIDTH
+//             placeholder: BLURRED
+//             width: 424
+//             height: 212
+//           )
+//         }
+//         description {
+//           raw
+//         }
+//       }
+//     }
+//   }
+// `
